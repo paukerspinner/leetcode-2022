30c818f8ce617eb03729f10bc19d7d3ee9128f97
pauker <xuancanhpham97@gmail.com>
2022-07-02 14:16:32 +0700

    resolved squares sorted array

---
+++ squares-sorted-array/index.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        result = []
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            if abs(nums[left]) > abs(nums[right]):
+                result = [nums[left] ** 2] + result
+                left += 1
+            else:
+                result = [nums[right] ** 2] + result
+                right -= 1
+
+        return [nums[left] ** 2] + result
+
+def sortedSquares(nums):
+    solution = Solution()
+    result = solution.sortedSquares(nums)
+    return result
+
+assert sortedSquares([-4,-1,0,3,10]) == [0,1,9,16,100], 'First'
+assert sortedSquares([-7,-3,2,3,11]) == [4,9,9,49,121], 'Second'
+assert sortedSquares([-4]) == [16], 'One element'
+assert sortedSquares([-2, -1]) == [1, 4], 'Two elements'
+assert sortedSquares([1,2,3,4,5]) == [1,4,9,16,25], 'Only positive numbers'
+assert sortedSquares([-5,-4,-3,-2,-1]), 'Only negative numbers'
