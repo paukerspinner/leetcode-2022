5e609c7e2739ab3b1e5bdb73cdfd62e2bae989de
pauker <xuancanhpham97@gmail.com>
2022-07-08 00:27:47 +0700

    refactor merge two binary trees

--- 617-merge-two-binary-trees/index.py
+++ 617-merge-two-binary-trees/index.py
@@ -9,10 +9,13 @@ class TreeNode:
 
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        if root1 is None and root2 is None:
-            return None
-        val = (root1.val if root1 else 0) + (root2.val if root2 else 0)
-        left = self.mergeTrees(root1.left if root1 else None, root2.left if root2 else None)
-        right = self.mergeTrees(root1.right if root1 else None, root2.right if root2 else None)
-        return TreeNode(val, left, right)
+        if root1 is None:
+            return root2
+        if root2 is None:
+            return root1
+        
+        left = self.mergeTrees(root1.left, root2.left)
+        right = self.mergeTrees(root1.right, root2.right)
+        return TreeNode(root1.val + root2.val, left, right)
+        
         
