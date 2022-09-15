0b84a29886bfd4c5ddb1e9cf6ac3d5ef9b9434a4
pauker <xuancanhpham97@gmail.com>
2022-09-15 21:23:03 +0700

    remove duplicated code

--- 300-longest-increasing-subsequence/index.py
+++ 300-longest-increasing-subsequence/index.py
@@ -1,6 +1,5 @@
 # dynamic programming greedy
 from typing import List
-
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         maxLenTo = []
@@ -13,19 +12,6 @@ class Solution:
             maxLenTo.append(prevMax + 1)
 
         return max(maxLenTo)
-
-def lengthOfLIS(nums: List[int]) -> int:
-    print('***************************')
-    print(nums)
-    sls = Solution()
-    result = sls.lengthOfLIS(nums)
-    print('--------------')
-    print(result)
-    return result
-
-assert lengthOfLIS([10,9,2,5,3,7,101,18]) == 4
-assert lengthOfLIS([0,1,0,3,2,3]) == 4
-assert lengthOfLIS([7,7,7,7,7,7,7]) == 1
 
 def lengthOfLIS(nums: List[int]) -> int:
     print('***************************')
